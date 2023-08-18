f48a01a18e5bf8eb6a2bf709498c8a2c3efe3d15
alexleethinker <alexleethinker@gmail.com>
2023-08-18 14:10:38 +0000

    updates

---
+++ DRLTrader/FeatureEngineer.py
@@ -0,0 +1,18 @@
+
+from finrl.meta.preprocessor.preprocessors import FeatureEngineer
+from DRLTrader.Config import INDICATORS, DATA_SAVE_DIR
+import numpy as np
+
+
+def feature_engieering(df):
+
+    fe = FeatureEngineer(use_technical_indicator=True,
+                        tech_indicator_list = INDICATORS,
+                        use_turbulence=True,
+                        user_defined_feature = False)
+    processed = fe.preprocess_data(df)
+    processed = processed.copy()
+    processed = processed.fillna(0)
+    processed = processed.replace(np.inf,0)
+
+    return processed
